8c00770a8d00e7cd0f51d3692c93147f76f42faf
gabrielsiq <gabrielsiq@msn.com>
2018-06-08 22:09:46 -0700

    Deleted old functions

--- pyConAVL.py
+++ pyConAVL.py
@@ -30,7 +30,6 @@ class ConAVL(object):
 
     def put(self, dkey, dval = None):
         self.__putNode(self.root, dkey, str(dkey) if dval is None else dval)
-        self.__rebalance()
 
     def min(self):
         return self.__getMinNode(self.root)
@@ -40,15 +39,9 @@ class ConAVL(object):
 
     def remove(self, dkey):
         self.__removeNode(self.root, dkey)
-        self.__rebalance()
 
     def print(self):
         self.__prettyPrintTree(self.root)
-
-    def __rebalance(self):
-        unbalanced = self.__balanceCheck(self.root)
-        if unbalanced is not None:
-            self.root = self.__getRoot(self.__autoRotate(unbalanced))
         
     def __str__(self):
         return strTree(self.root)
@@ -462,170 +455,6 @@ class ConAVL(object):
             return nL
 
         return self.__fixHeight(nParent)
-
-    def __balanceCheck(self, droot):
-        """
-        should check after every modification
-        return imbalanced node for rotation,
-        otherwise return None
-        """
-        dnode = droot
-        if droot is None:
-            return None
-
-        nl = dnode.left.height if dnode.left != None else -1
-        nr = dnode.right.height if dnode.right != None else -1
-        RR = None
-        if abs(nl - nr) > 1:
-            if nl * nr <= 0:
-                # unbalanced node: direct return
-                return dnode
-            else:
-                RR = dnode
-        Rl = None
-        Rr = None
-        if dnode.left != None:
-            Rl = self.__balanceCheck(dnode.left)
-        if Rl != None:
-            return Rl
-        if dnode.right != None:
-            Rr = self.__balanceCheck(dnode.right)
-        if Rr != None:
-            return Rr
-        return RR
-
-    def __rotateLL(self, dnode):
-        """
-        base type
-        rotate LL type, return root node of subtree
-        """
-        k2 = dnode
-        k1 = dnode.left
-        y = k1.right
-        if k2.parent != None:
-            p = k2.parent
-            if p.left != None and p.left.key == k2.key:
-                p.left = k1
-            else:
-                p.right = k1
-            k1.parent = p
-        else:
-            k1.parent = None
-
-        k2.left = y
-        if y != None:
-            y.parent = k2
-
-        k1.right = k2
-        k2.parent = k1
-
-        # fix heights here
-        k2.height = max(
-            k2.left.height if k2.left != None else -1,
-            k2.right.height if k2.right != None else -1
-        ) + 1
-        k1.height = max(
-            k1.left.height if k1.left != None else -1,
-            k1.right.height if k1.right != None else -1
-        ) + 1
-
-        return k1
-
-    def __rotateLR(self, dnode):
-        """
-        complex type
-        rotate LR type, return root node of subtree
-        """
-        k3 = dnode
-        k1 = k3.left
-
-        # first do an RR on k1
-        self.__rotateRR(k1)
-        # then do an LL on k3, no need to fix height
-        return self.__rotateLL(k3)
-
-    def __rotateRL(self, dnode):
-        """
-        complex type
-        rotate RL type, return root node of subtree
-        """
-        k1 = dnode
-        k3 = k1.right
-        k2 = k3.left
-
-        # first do an LL on k3
-        k2 = self.__rotateLL(k3)
-        # then do an RR on k1
-        k2 = self.__rotateRR(k1)
-        # no need to fix height
-        return k2
-
-    def __rotateRR(self, dnode):
-        """
-        base type
-        rotate RR type, return root node of subtree
-        """
-        k1 = dnode
-        k2 = dnode.right
-        y = k2.left
-        if k1.parent != None:
-            p = k1.parent
-            if p.left != None and p.left.key == k1.key:
-                p.left = k2
-            else:
-                p.right = k2
-            k2.parent = p
-        else:
-            k2.parent = None
-
-        k1.right = y
-        if y != None:
-            y.parent = k1
-
-        k2.left = k1
-        k1.parent = k2
-
-        # fix heights here
-        k1.height = max(
-            k1.left.height if k1.left != None else -1,
-            k1.right.height if k1.right != None else -1
-        ) + 1
-        k2.height = max(
-            k2.left.height if k2.left != None else -1,
-            k2.right.height if k2.right != None else -1
-        ) + 1
-
-        return k2
-
-    def __autoRotate(self, dnode):
-        """
-        check and determine the type of rotation, and do the rotation
-        """
-        nl = dnode.left.height if dnode.left != None else -1
-        nr = dnode.right.height if dnode.right != None else -1
-
-        if nl < nr:
-            # R*, nr>=0 must hold
-            ddnode = dnode.right
-            nnl = ddnode.left.height if ddnode.left != None else -1
-            nnr = ddnode.right.height if ddnode.right != None else -1
-            if nnl < nnr:
-                # print("RR")
-                return self.__rotateRR(dnode)
-            else:
-                # print("RL")
-                return self.__rotateRL(dnode)
-        else:
-            # L*, nl>=0 must hold
-            ddnode = dnode.left
-            nnl = ddnode.left.height if ddnode.left != None else -1
-            nnr = ddnode.right.height if ddnode.right != None else -1
-            if nnl < nnr:
-                # print("LR")
-                return self.__rotateLR(dnode)
-            else:
-                # print("LL")
-                return self.__rotateLL(dnode)
 
     def __getRoot(self, dnode):
         """
