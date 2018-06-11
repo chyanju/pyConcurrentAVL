7a9484fa7e1ac0ce7c3296b755e187e728ff688b
chyanju <chyanju@gmail.com>
2018-06-10 17:39:18 -0700

    update strTree call

--- pyConAVL.py
+++ pyConAVL.py
@@ -53,7 +53,7 @@ class ConAVL(object):
         self.__prettyPrintTree(self.root)
         
     def __str__(self):
-        return strTree(self.root)
+        return strTree(self.root.right)
 
     def __getNode(self, droot, dkey):
         """
@@ -746,7 +746,7 @@ def strTree(droot):
             return dstree
         else:
             dstree += ddnode.val
-        if ddnode.height > 0:
+        if ddnode.height > 1:
             dstree += "("
             dstree = DFSNode(ddnode.left, dstree)
             dstree += ","
